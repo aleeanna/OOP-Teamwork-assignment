53609d74e9c02500c67542a10d5d2388fd6d1dfb
aleeanna <helenebolpachova@gmail.com>
2015-06-15 02:12:09 +0300

    Enemy class

---
+++ ZeGame1.0/Kikimora.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZeGame1._0
+{
+    class Kikimora : Enemy
+    {
+        private const int expriencePoints = 40;
+        private const int healthPoints = 80;
+
+        public Kikimora(string creatureType, int hitPoints, int attackPoints, int defensePoints, int experiencePoints) : base( creatureType,  hitPoints,  attackPoints,  defensePoints,  experiencePoints)
+        {
+            
+        }
+    }
+}
